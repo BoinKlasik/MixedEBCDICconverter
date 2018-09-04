1f8c4816f60e75f70323a704a35d464acb50a892
BoinKlasik <BoinKlasik@gmail.com>
2018-09-03 18:24:13 -0700

    Started district record

--- doeet.py
+++ doeet.py
@@ -2,16 +2,32 @@
 import os
 import struct
 
+def read_int(byte_input, offset):
+    return struct.unpack(">i", byte_input[offset:4]), offset + 4
 
 class RecordDataWord:
     def __init__(self, byte_input : bytes):
         if len(byte_input) is not 4:
             raise ValueError("RDW is only 4 bytes long")
-        self.length = struct.unpack("<h")
+        self.length = struct.unpack(">h", byte_input[:2])[0]
 
 
-def readRecord(byteinput) :
-    rdw = RecordDataWord(byteinput[:4])
+class DistrictDataRecord:
+    def __init__(self, byte_input : bytes):
+        offset = 0
+        self.school_system_code, offset = read_int(byte_input, offset)
+        self.system_name = byte_input[8:31].decode(encoding="cp1140")
+        self.address = ""
+
+
+def read_record(byte_stream) :
+    rdw_bytes = byte_stream.read(4)
+    if not len(rdw_bytes):
+        return None
+    rdw = RecordDataWord(rdw_bytes)
+    record_type = struct.unpack(">B", byte_stream.read(1))[0]
+    if record_type is 1:
+        ddr = DistrictDataRecord(byte_stream.read(rdw.length - 5))
     print(rdw.length)
     return rdw.length
 
@@ -19,9 +35,5 @@ inputfiles = os.listdir('input')
 print(inputfiles)
 for fileName in inputfiles:
     with open('input/' + fileName, mode='br') as input_file:
-        input_binary = input_file.read()
-        ebcdic_decoded = input_binary.decode(encoding='cp1140')
-        with open('output/' + fileName + ".output", mode='w', encoding='cp1140') as output_file:
-            byte_encoded = bytes(ebcdic_decoded, encoding='utf-8')
-            output_file.write(ebcdic_decoded)
+        read_record(input_file)
     break
